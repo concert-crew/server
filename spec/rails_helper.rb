dd78cf77330beb3b25dfa3dab1c7c13ef6c25990
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-10-13 12:44:34 -0500

    Add model tests for User

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,14 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
