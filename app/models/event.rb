9c7c8723ca7e1840062c8e35168ae24bb74ac061
Shirley DeCesari <shirley.decesari@gmail.com>
2022-10-17 15:08:50 -0700

    Update model test for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,9 @@
 class Event < ApplicationRecord
-  validates_presence_of :artist_name
-  validates_presence_of :venue_name
-  validates_presence_of :venue_location
+  validates_presence_of :name
   validates_presence_of :date
+  validates_presence_of :image
+  validates_presence_of :_embedded
+  validates_presence_of :url
 
   has_many :user_events
   has_many :users, through: :user_events
