53bd49af41353d600f9267fe917d60455c9012a8
John Santosuosso <john.santosuosso1@gmail.com>
2022-10-17 17:45:29 -0400

    add unupdated commits

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,5 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins 'http://localhost:3000', 'https://roed-to-aide-fe.herokuapp.com/'
     origins '*'
 
     resource '*',
