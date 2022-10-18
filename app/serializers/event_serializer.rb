2e4d815a439810de718d89a127c947debb56519d
John Santosuosso <john.santosuosso1@gmail.com>
2022-10-18 16:15:51 -0400

    setup serializer to display requested data

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,13 +1,23 @@
 class EventSerializer
   class << self
     def new(events)
-      require 'pry'; binding.pry 
       {
-        data: {
-          id: nil,
-          type: 'event',
-          attributes: {
+        data: { 
+          events: events.map do |event| {
+            id: event.id,
+            name: event.name,
+            buyTicketsURL: event.buyTicketsUrl,
+            image: event.image,
+            date: event.date,
+            time: event.time,
+            venueName: event.venueName,
+            city: event.city,
+            state: event.state,
+            address: event.address,
+            longitude: event.longitude,
+            latitude: event.latitude
           }
+          end
         }
       }
     end
