bca44b9b01da1222ee5eca1c53de048f80f863a1
Shirley DeCesari <shirley.decesari@gmail.com>
2022-10-17 18:05:03 -0700

    Update date datatype in database

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_17_213810) do
+ActiveRecord::Schema.define(version: 2022_10_18_010346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
     t.string "name"
-    t.datetime "date"
+    t.string "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
