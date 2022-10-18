9f9acb1e4dc202c4e8000127edb33cad54e8cbb5
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-10-18 16:13:44 -0600

    add has many 'friends' attribute to User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   validates_presence_of :name
-  validates_presence_of :image, allow_nil: true 
+  validates_presence_of :image, allow_nil: true
 
   has_many :user_events
   has_many :events, through: :user_events
+
+  has_many :friends, class_name: "User", foreign_key: "friend_id"
 end
