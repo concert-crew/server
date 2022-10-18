963cac1601f4c006ba4351813183fbb84161fc40
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-10-18 14:16:21 -0500

    Modify seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,27 @@
-abby = User.create!(
-  name: 'Abby', 
-  image: 'https://avatars.githubusercontent.com/u/100726140?s=400&u=fefcffaec4d464cc411254317c47b087ab504c42&v=4')
-chantal = User.create!(
-  name: 'Chantal',
-  image: 'https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png'
-)
-mayu = User.create!(
-  name: 'Mayu',
-  image: 'https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png'
-)
-shirley = User.create!(
-  name: 'Shirley',
-  image: 'https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png' 
-)
-john = User.create!(
-  name: 'John',
-  image: 'https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png'
-)
+  abby = User.create!(
+    name: 'Abby', 
+    image: 'https://avatars.githubusercontent.com/u/100726140?s=400&u=fefcffaec4d464cc411254317c47b087ab504c42&v=4'
+  )
+
+  chantal = User.create!(
+    name: 'Chantal',
+    image: 'https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png'
+  )
+
+  mayu = User.create!(
+    name: 'Mayu',
+    image: 'https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png'
+  )
+
+  shirley = User.create!(
+    name: 'Shirley',
+    image: 'https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png' 
+  )
+
+  john = User.create!(
+    name: 'John',
+    image: 'https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png'
+  )
 
 5.times do 
   Event.create!(
@@ -36,6 +41,8 @@ john = User.create!(
 end
 
 Event.all.each do |event| 
-  event.user_events.create!(user_id: user.id, event_id: event.id)
+  User.all.each do |user| 
+    event.user_events.create!(user_id: user.id, event_id: event.id)
+  end
 end
 
