e91ef67d47cc358d4d39ae3ec916533ea1eb9903
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-10-18 16:11:13 -0600

    test User has many friends

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,5 +9,6 @@ RSpec.describe User do
   describe 'relationships' do 
     it { should have_many :user_events }
     it { should have_many(:events).through(:user_events) }
+    it { should have_many(:friends) }
   end 
 end
