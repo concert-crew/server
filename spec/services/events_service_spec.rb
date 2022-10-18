5b9e4ebfcb7901093a34134f901fc245e97f15c5
John Santosuosso <john.santosuosso1@gmail.com>
2022-10-18 13:26:45 -0400

    service testing for event complete, happy path

--- spec/services/events_service_spec.rb
+++ spec/services/events_service_spec.rb
@@ -5,5 +5,20 @@ RSpec.describe EventsService do
     response = EventsService.call_for_events('Seattle')
     require 'pry'; binding.pry 
     expect(response).to be_a(Hash)
+    expect(response[:_embedded][:events].first).to have_key(:name)#event name
+    expect(response[:_embedded][:events].first).to have_key(:id)#ticketmaster event ID
+    expect(response[:_embedded][:events].first).to have_key(:url)#buy ticketsURL
+    expect(response[:_embedded][:events].first).to have_key(:images)#images
+    expect(response[:_embedded][:events].first[:dates][:start]).to have_key(:localDate)#event date
+    expect(response[:_embedded][:events].first[:dates][:start]).to have_key(:localTime)#event time
+    expect(response[:_embedded][:events].first).to have_key(:_embedded)
+    expect(response[:_embedded][:events].first[:_embedded]).to have_key(:venues)
+    expect(response[:_embedded][:events].first[:_embedded][:venues].first).to have_key(:name)#Venue name
+    expect(response[:_embedded][:events].first[:_embedded][:venues].first[:city]).to have_key(:name)#Venue city
+    expect(response[:_embedded][:events].first[:_embedded][:venues].first[:state]).to have_key(:name)#Venue state
+    expect(response[:_embedded][:events].first[:_embedded][:venues].first[:address]).to have_key(:line1)#Venue address
+    expect(response[:_embedded][:events].first[:_embedded][:venues].first[:location]).to have_key(:longitude)#Event longitude
+    expect(response[:_embedded][:events].first[:_embedded][:venues].first[:location]).to have_key(:latitude)#Event latitude
+    expect(response[:_embedded][:events].count).to eql(10)
   end
 end
