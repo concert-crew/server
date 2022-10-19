d945e1c23d56e2b53b4a26286bc678779cf5b8ca
John Santosuosso <john.santosuosso1@gmail.com>
2022-10-18 20:25:08 -0400

    add vcr to facades

--- spec/facades/events_facade_spec.rb
+++ spec/facades/events_facade_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe EventsFacade do 
-  it 'creates poro for event data' do
+  it 'creates poro for event data', :vcr do
     events = EventsFacade.get_events('Seattle')
     expect(events).to be_a(Array)
   end
