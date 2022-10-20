a9eef316eb051db89c56696fe449b1ff15e37d98
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-10-20 15:42:31 -0600

    add friends reference to User table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  validates_presence_of :name
-  validates_presence_of :image, allow_nil: true
-
   has_many :user_events
   has_many :events, through: :user_events
+  
+  has_many :friends, ->(user) { where("follower_id = ? OR followed_id = ?", user.id, user.id)}
 
-  has_many :friends, class_name: "User", foreign_key: "friend_id"
+  validates_presence_of :name
+  validates_presence_of :image, allow_nil: true
 end
