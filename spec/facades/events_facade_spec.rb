b3d4e0edd0d8ba64c7febe442616cfa996374f4b
John Santosuosso <john.santosuosso1@gmail.com>
2022-10-21 13:46:35 -0400

    add event_data.json in fixtures file implement sad path testing in facade that reads event_data.json file remove attributes from event_data.json file to ensure nil or missing values are accounted for

--- spec/facades/events_facade_spec.rb
+++ spec/facades/events_facade_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe EventsFacade do
     events = EventsFacade.get_events('Seattle')
     expect(events).to be_a(Array)
   end
+
+  it 'creates poro for event data, sad path, missing attributes, name, longitude or latitude', :vcr do
+    events = File.read('spec/fixtures/event_data.json')
+    events = JSON.parse(events, symbolize_names: true)
+    all_events = events[:_embedded][:events].map do |event|
+      PoroEvent.new(event)
+    end
+  end
 end
