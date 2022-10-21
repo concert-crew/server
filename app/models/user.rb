b1c4815fb794faee6e3013a69a08a607ca4441a7
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-10-21 12:35:35 -0600

    rename followed_id to receiver_id and follower_id to requestor_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :user_events
   has_many :events, through: :user_events
   
-  has_many :friends, ->(user) { where("follower_id = ? OR followed_id = ?", user.id, user.id)}
+  has_many :friends, ->(user) { where("receiver_id = ? OR requestor_id = ?", user.id, user.id) }
 
   validates_presence_of :name
   validates_presence_of :image, allow_nil: true
