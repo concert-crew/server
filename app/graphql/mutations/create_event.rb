26a63581189bdb911561debb356b983e927a0996
Shirley DeCesari <shirley.decesari@gmail.com>
2022-10-21 15:44:42 -0700

    Remove user_event field from create_event mutation and delete user_event_type file

--- app/graphql/mutations/create_event.rb
+++ app/graphql/mutations/create_event.rb
@@ -14,14 +14,13 @@ class Mutations::CreateEvent < Mutations::BaseMutation
   argument :user_id, Integer, required: true
 
   field :event, Types::EventType, null: false 
-  field :user_event, Types::UserEventType, null: false
   field :errors, [String], null: false 
 
   def resolve(name:, date:, image:, buy_tickets_url:, time:, venue_name:, city:, state:, address:, longitude:, latitude:, ticketmaster_id:, user_id:)
     event = Event.new(name: name, date: date, image: image, buy_tickets_url: buy_tickets_url, time: time, venue_name: venue_name, city: city, state: state, address: address, longitude: longitude, latitude: latitude, ticketmaster_id: ticketmaster_id)
 
     if event.save
-      user_event = UserEvent.create(user_id: user_id, event_id: event.id) 
+      user_event = UserEvent.new(user_id: user_id, event_id: event.id) 
       {
         event: event,
         user_event: user_event, 
--- app/graphql/types/user_event_type.rb
+++
@@ -1,8 +0,0 @@
-module Types
-  class UserEventType < Types::BaseObject
-    field :id, ID, null: false
-    field :user_id, String
-    field :event_id, String
-  end 
-end 
-
