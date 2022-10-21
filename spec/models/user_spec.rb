4183d689bef1cfd6200573e0010c5ba14cde5c8d
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-10-21 14:53:34 -0600

    test #friendships joins a user to another user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,26 @@ RSpec.describe User do
   end
 
   describe 'relationships' do 
-    it { should have_many :user_events }
+    it { should have_many(:user_events) }
     it { should have_many(:events).through(:user_events) }
   end 
+
+  describe "#friendships" do
+    it 'creates friendship association by user id' do
+      chantal = User.create!(
+        name: 'Chantal',
+        image: 'https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png'
+      )
+
+      abby = User.create!(
+        name: 'Abby', 
+        image: 'https://avatars.githubusercontent.com/u/100726140?s=400&u=fefcffaec4d464cc411254317c47b087ab504c42&v=4'
+      )
+      
+      friendship1 = Friend.create!(receiver_id: chantal.id, requestor_id: abby.id)
+      
+      expect(abby.friendships).to eq([friendship1])
+    end
+  end
 end
+    
