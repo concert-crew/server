2bb2d7883824709527a110b8297e317122f15412
Shirley DeCesari <shirley.decesari@gmail.com>
2022-10-24 09:23:55 -0700

    Add existing user logic
    Co-authored-by: Rue <rzheng13@gmail.com>
    Co-authored-by: Mayu <mayu.takeda.utd@gmail.com>

--- app/graphql/mutations/create_event.rb
+++ app/graphql/mutations/create_event.rb
@@ -17,15 +17,21 @@ class Mutations::CreateEvent < Mutations::BaseMutation
   field :errors, [String], null: false 
 
   def resolve(name:, date:, image:, buy_tickets_url:, time:, venue_name:, city:, state:, address:, longitude:, latitude:, ticketmaster_id:, user_id:)
-    exisiting_event = Event.find_by(name: name, date: date, city:city)
+    existing_event = Event.find_by(name: name, date: date, city:city)
+    existing_user = UserEvent.find_by(user_id: user_id, event_id: existing_event.id)
 
-    if exisiting_event 
+    if existing_event && existing_user
+      {
+        event: existing_event,
+        errors: []
+      }
+    elsif existing_event 
       UserEvent.create(user_id: user_id, event_id: existing_event.id)
       {
         event: existing_event,
         errors: []
       }
-    elsif exisiting_event == nil
+    elsif existing_event == nil
       event = Event.create(name: name, date: date, image: image, buy_tickets_url: buy_tickets_url, time: time, venue_name: venue_name, city: city, state: state, address: address, longitude: longitude, latitude: latitude, ticketmaster_id: ticketmaster_id)
       UserEvent.create(user_id: user_id, event_id: event.id)
       {
