445f41345713efe66da9ee75030238016ec76099
Shirley DeCesari <shirley.decesari@gmail.com>
2022-10-25 15:43:44 -0700

    Add tests for user type queries

--- spec/graphql/queries/events/get_users_spec.rb
+++ spec/graphql/queries/events/get_users_spec.rb
@@ -5,7 +5,15 @@ module Queries
     RSpec.describe Types::QueryType do 
       describe '.resolve' do 
         it 'returns all users' do 
-          
+          user1 = User.create!(name: "Abby", image: "url")
+          user2 = User.create!(name: "Chantal", image: "url")
+          user3 = User.create!(name: "Mayu", image: "url")
+
+          result = ServerSchema.execute(query).as_json
+          expect(result['data']['users'].count).to eq(3)
+          expect(result['data']['users'][0]['name']).to eq('Abby')
+          expect(result['data']['users'][1]['name']).to eq('Chantal')
+          expect(result['data']['users'][2]['name']).to eq('Mayu')
         end 
 
         def query
